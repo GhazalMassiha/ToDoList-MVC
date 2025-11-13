0c78d6b79f50f3bd5d826ea908fdd9ed5482d3c3
Ghazal Massiah <GhazalMassiah@users.noreply.github.com>
2025-11-14 00:36:03 +0330

    Add project files.

---
+++ src/01.Domain/Core-ToDoList.Domain.Core/ToDoList.Domain.Core/_common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ToDoList.Domain.Core._common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
